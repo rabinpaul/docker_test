2713e0ce12b64cac15988b0303712762dcc4b642
rabinpaul <rabinchandrapaul@gmail.com>
2020-05-11 19:51:46 +0530

    Initial Commit

---
+++ go_channel/test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello(dat chan int) {
+
+	time.Sleep(4 * time.Second)
+	fmt.Println("In Hello")
+
+	dat <- 2
+}
+
+func main() {
+	data := make(chan int)
+	go hello(data)
+	a := <-data
+
+	fmt.Println("main Function", a)
+}
